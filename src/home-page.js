0cee3d67ff4da607c004a6ca0d9f13cab107d581
Elkanah <elkanahawesome@gmail.com>
2023-03-17 16:50:08 -0500

    Add: rating section and changed font
    Add: section with ratings
    
    Add: new images to Image folder
    Add: new Font
    Add: styling to rating section
    Add: new font to texts

--- src/home-page.js
+++ src/home-page.js
@@ -2,15 +2,13 @@ import diningArea from "./Images/k8-sWEpcc0Rm0U-unsplash.jpg";
 import createElement, { addRatingStars } from "./helperFunctions";
 import { header, main, footer, homeTab } from "./initial-page-load";
 import ratImage from "./Images/RATTTTTT.jpg";
-
+import mrPotato from "./Images/larryCucumberInDiningPLace.jpeg";
+import bobTheTomato from "./Images/bobTheTomato.jpg";
 // remove any highlight on tab button that is not home
 // highlight home tab button when clicked
 // remove content so that home tab contents can be filled
 
 // TODO ADD REVIEW SECTION WITH REVIEWS
-const reviewProfileHolder = createElement("div", {
-  class: "review-profile-holder",
-});
 
 const mainImage = createElement("img", {
   src: diningArea,
@@ -18,9 +16,25 @@ const mainImage = createElement("img", {
   class: "background-image",
 });
 
-const profilePicture = createElement("img", {
+const homePageSection = createElement("div", {
+  class: "home-page-section",
+});
+
+const profilePicture1 = createElement("img", {
   src: ratImage,
   alt: "Rat cooking LETHIMCOOK",
+  class: "review-profile-picture",
+});
+
+const profilePicture2 = createElement("img", {
+  src: bobTheTomato,
+  alt: "Bob The Tomato Wearing A Hat Profile Picture",
+  class: "review-profile-picture",
+});
+
+const profilePicture3 = createElement("img", {
+  src: mrPotato,
+  alt: "Mr. Potato Profile Picture",
   class: "review-profile-picture",
 });
 
@@ -42,7 +56,35 @@ const [reviewCard1, reviewCard2, reviewCard3] = [
   createElement("div", { class: "review-card" }),
 ];
 
-const reviewComment = createElement("p", { class: "review-comment" });
+/// TODO
+/* function createReviewCard(img, rating, comment, personName) {
+  // create container 
+  const reviewCard = createElement("div", { class: "review-card" });
+
+  // create profile image container
+  const profileImageContainer = createElement("div", {
+    class: "review-profile-holder",
+  });
+
+  // create image 
+const profilePicture = createElement("img", {
+  src: img[0],
+  alt: img[1],
+  class: img[2],
+});
+
+  // create container to house the rating stars and comments
+
+  // create rating
+  addRatingStars(rating)
+
+  // create comment container
+
+  // create name 
+
+  
+
+} */
 
 // Set the elements text contents
 
@@ -50,31 +92,63 @@ homePageText1.innerText = "Worst food in the world!";
 homePageText2.innerText = "- Made by unpaid workers.";
 /* homePageText3.innerText = "Dont eat here truse me"; */
 orderButton.innerText = "Order Now";
-reviewComment.innerText = "I died from eating this food 11/10";
+/* reviewComment.innerText = "I died from eating this food 11/10"; */
 
 // Append elements inside one another
 
 homePageTextContents.append(homePageText1, homePageText2);
-homePageContents.appendChild(homePageTextContents);
-homePageContents.appendChild(orderButton);
+homePageContents.append(homePageTextContents, orderButton);
 
+/* reviewCard1.appendChild(createElement(profileImageContainer).appendChild(profilePicture1)) 
+reviewCard2.appendChild(createElement(profileImageContainer).appendChild(profilePicture2)) 
+reviewCard3.appendChild(createElement(profileImageContainer).appendChild(profilePicture3)) */
+
+let index = 0;
 [reviewCard1, reviewCard2, reviewCard3].forEach((element) => {
-  const reviewProfileClone = reviewProfileHolder.cloneNode(true);
-  const profilePictureClone = profilePicture.cloneNode(true);
-  const reviewCommentClone = reviewComment.cloneNode(true);
+  const profileImageContainer = createElement("div", {
+    class: "review-profile-holder",
+  });
+
+  const profileImage = [profilePicture1, profilePicture2, profilePicture3];
+
+  // create container for comment
+  const reviewComment = createElement("p", { class: "review-comment" });
+
+  // create strings of text for comment container
+  const comments = [
+    "I died from eating this food 11/10",
+    "He served me a cucumber for some reason it taste my friend larry idk why",
+    "Yo the staff are giving me tour of their back kitchen they said they have something special for me we lit",
+  ];
+
+  const reviewersName = ["Rattaouli", "Bob The Tomato", "Larry The Cucumber"];
+
+  // review container to contain the rating of stars and the comment
   const reviewContainer = createElement("div", {
     class: "review-container",
   });
-  reviewContainer.append(addRatingStars(5), reviewCommentClone);
 
-  reviewProfileClone.appendChild(profilePictureClone);
-  element.append(reviewProfileClone, reviewContainer);
+  // append review comment inside
+  reviewComment.innerText = comments[index];
+
+  // append profile image to review container
+  profileImageContainer.appendChild(profileImage[index]);
+
+  // create stars and append to review container
+  reviewContainer.append(addRatingStars(5), reviewComment);
+
+  // append image to profile container
+  profileImageContainer.appendChild(profileImage[index]);
+
+  element.append(profileImageContainer, reviewContainer);
+
+  ++index;
 });
 
 reviews.append(reviewCard1, reviewCard2, reviewCard3);
 
 export default function loadHomePageContents() {
   /// do stuff
-
-  main.append(mainImage, homePageContents, reviews);
+  homePageSection.append(mainImage, homePageContents);
+  main.append(homePageSection, reviews);
 }
