1249aaacb7198fb2b656113b9be7b957dec86f5b
Elkanah <elkanahawesome@gmail.com>
2023-05-19 16:51:02 -0500

    Refactor: moved home/menu/contact tab addEventListener to "index.js" to prevent dependency cycle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import firstLoad, { contactTab } from "./initial-page-load";
+import firstLoad, { homeTab, menuTab, contactTab } from "./initial-page-load";
+import loadHomePageContents from "./home-page";
+import menuPage from "./menu-tab";
 import contactPage from "./contact-tab";
 
 import "./style.css";
 
 firstLoad();
 
+homeTab.addEventListener("click", loadHomePageContents);
+menuTab.addEventListener("click", menuPage);
 contactTab.addEventListener("click", contactPage);
---
+++ src/menu-tab.js
@@ -0,0 +1,13 @@
+import removePageContents from "./removeContents";
+
+export default function menuPage() {
+  removePageContents();
+}
+
+/* TODO
+FIND A GOOD IMAGE FOR A MENU 
+
+ADD MENU ITEMS
+
+
+*/
