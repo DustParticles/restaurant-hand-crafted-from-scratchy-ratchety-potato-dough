131368f9dbb13abeff82d27fd41418d48f3194ac
Elkanah <elkanahawesome@gmail.com>
2023-05-25 08:24:38 -0500

    Add: menu to contain the food items options Add: styling to fonts, layout, colors to "menu-tab.js"

--- src/menu-tab.js
+++ src/menu-tab.js
@@ -3,6 +3,58 @@ import removePageContents from "./removeContents";
 import { createElement } from "./helperFunctions";
 import diningTableImage from "./Images/diningTable.jpg";
 
+const menuPageContainer = createElement("div", {
+  class: "menu-page-container",
+});
+
+const menuContainer = createElement("div", {
+  class: "menu-container",
+});
+
+const menuTitle = createElement("p", {
+  class: "menu-title",
+});
+
+const menuSection1 = createElement("div", {
+  class: "menu-section menu-section-1",
+});
+
+const menuSection2 = createElement("div", {
+  class: "menu-section menu-section-2",
+});
+
+const menuItem1 = createElement("div", {
+  class: "menu-item-container",
+});
+
+const menuItemTitle = createElement("p", {
+  class: "menu-item-title ",
+});
+
+const menuItemPrice = createElement("p", {
+  class: "menu-item-price",
+});
+
+const menuItemTitleContainer = createElement("div", {
+  class: "menu-title-container",
+});
+
+const menuItemDescription = createElement("p", {
+  class: "menu-item-Description",
+});
+
+menuItemPrice.innerText = "$69.99";
+menuItemTitle.innerText = "HUMAN MEAT";
+menuItemDescription.innerText = "MADE FROM THE FINEST HUMANS";
+
+menuItemTitleContainer.append(menuItemTitle, menuItemPrice);
+menuItem1.append(menuItemTitleContainer, menuItemDescription);
+
+menuTitle.innerText = "Menu";
+menuSection1.append(menuItem1.cloneNode(true));
+menuSection2.append(menuItem1.cloneNode(true));
+menuContainer.append(menuTitle, menuSection1, menuSection2);
+
 const backgroundImageContainer = createElement("div", {
   class: "menu-background-image-container",
 });
@@ -16,15 +68,23 @@ const diningTableBackgroundImage = createElement("img", {
 
 backgroundImageContainer.append(diningTableBackgroundImage);
 
+menuPageContainer.append(backgroundImageContainer, menuContainer);
+
 export default function menuPage() {
   removePageContents();
-  main.append(backgroundImageContainer);
+  main.append(menuPageContainer);
 }
 
 /* TODO
-FIND A GOOD IMAGE FOR A MENU 
+1. FIND A GOOD IMAGE FOR A MENU - DONE
 
-ADD MENU ITEMS
+2. CREATE A FUNCTION THAT CREATES ITEMS IN MENU
+  - FIGURE OUT WHAT DESIGN IS BEST
+  - DO 2 COLUMN FOOD MENU ITEMS?
+
+3. ADD MENU ITEMS
 
 
+
+MAKE MENU CONTAINER MAX WIDTH WAYYYYYY SMALLER!
 */
